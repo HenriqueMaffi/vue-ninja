4f3e676a6089ce42c21a9aa79720f377fa98cc52
Henrique Maffi <henrique.maffi@gmail.com>
2023-08-29 23:35:38 -0300

    first commit

---
+++ src/components/Results.vue
@@ -0,0 +1,37 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <p>Reaction time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return{
+      rank: null
+    }    
+  },
+  mounted() {
+    if(this.score < 250) {
+      this.rank = 'Ninja fingers'
+    } else if(this.score < 400) {
+      this.rank = 'Rapid reflexes'
+    } else {
+      this.rank = 'Snail pace...'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+
+</style>
